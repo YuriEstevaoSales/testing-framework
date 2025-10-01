a30611f1e6fe7340d97a1232fb4f7aef2782f9e5
Yuri <notoridake@proton.me>
2025-09-30 21:41:44 -0300

    5. Classe TestSuite

--- TestCase.py
+++ TestCase.py
@@ -23,20 +23,4 @@ class TestCase:
     def tear_down(self):
         pass
 
-class MyTest(TestCase):
-
-    def set_up(self):
-        print('set_up')
-
-    def tear_down(self):
-        print('tear_down')
-
-    def test_a(self):
-        print('test_a')
-
-    def test_b(self):
-        print('test_b')
-
-    def test_c(self):
-        print('test_c')
 
